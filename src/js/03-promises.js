82212b1d5fa00ea07153078f00171c5e90e2dd6a
DianaSachelarie <diana_sachelarie@yahoo.com>
2023-12-04 12:36:26 +0200

    Updated files

---
+++ src/js/03-promises.js
@@ -0,0 +1,47 @@
+const form = document.querySelector(".form");
+const firstDelay = document.querySelector("input[name=delay]");
+const delayStep = document.querySelector("input[name=step]");
+const amountInput = document.querySelector("input[name=amount]");
+
+form.addEventListener("submit", onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  let delay = Number(firstDelay.value);
+  let step = Number(delayStep.value);
+  let amount = Number(amountInput.value);
+  let position = 0;
+
+  for (let i = 1; i <= amount; i += 1) {
+    position = i;
+    if (i > 1) {
+      delay += step;
+    }
+    createPromise(position, delay).then(onSuccess).catch(onError);
+  }
+  form.reset();
+}
+
+function createPromise(position, delay) {
+  const promise = new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        // Fulfill
+        resolve({ position, delay });
+      } else {
+        // Reject
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+  return promise;
+}
+
+function onSuccess({ position, delay }) {
+  console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+}
+function onError({ position, delay }) {
+  console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+}
